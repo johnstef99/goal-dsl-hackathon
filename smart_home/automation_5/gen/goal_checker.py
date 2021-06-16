39fe9c855ed76be0c05d57358d52ac4b5325cf47
John Stef <johnstef1999@gmail.com>
2021-06-16 17:57:59 +0300

    fix recursion problem

--- smart_home/automation_5/gen/goal_checker.py
+++ smart_home/automation_5/gen/goal_checker.py
@@ -10,33 +10,62 @@ from goalee.types import Point
 if __name__ == '__main__':
     middleware = RedisMiddleware()
     t = Target(middleware, name='MyAppTarget',
-               score_weights=[0.25, 0.25, 0.25, 0.25])
+               score_weights=[0.3333333333333333, 0.3333333333333333, 0.3333333333333333])
 
     g = ComplexGoal(name='Kitchen',
                     max_duration=None,
                     min_duration=None)
+    gs = TopicMessageReceivedGoal(topic='kitchen.intruder_detected',
+                                  name='KitchenIntruder',
+                                  max_duration=None,
+                                  min_duration=None)
+
     g.add_goal(gs)
+    g.add_goal(gs)
+    g.add_goal(gs)
+    gs = TopicMessageParamGoal(topic='robot.on_enter',
+                               name='Kitchen',
+                               condition=lambda msg: True if msg['room'] == "kitchen" else False,
+                               max_duration=11.0,
+                               min_duration=6.0)
     g.add_goal(gs)
     ## More Goals to Generate here
     t.add_goal(g)
     g = ComplexGoal(name='Livingroom',
                     max_duration=None,
                     min_duration=None)
+    gs = TopicMessageReceivedGoal(topic='livingroom.intruder_detected',
+                                  name='LivingroomIntruder',
+                                  max_duration=None,
+                                  min_duration=None)
+
     g.add_goal(gs)
+    g.add_goal(gs)
+    g.add_goal(gs)
+    gs = TopicMessageParamGoal(topic='robot.on_enter',
+                               name='Livingroom',
+                               condition=lambda msg: True if msg['room'] == "livingroom" else False,
+                               max_duration=10.0,
+                               min_duration=5.0)
     g.add_goal(gs)
     ## More Goals to Generate here
     t.add_goal(g)
     g = ComplexGoal(name='Bedroom',
                     max_duration=None,
                     min_duration=None)
+    gs = TopicMessageReceivedGoal(topic='bedroom.intruder_detected',
+                                  name='BedroomIntruder',
+                                  max_duration=None,
+                                  min_duration=None)
+
     g.add_goal(gs)
     g.add_goal(gs)
-    ## More Goals to Generate here
-    t.add_goal(g)
-    g = ComplexGoal(name='Bathroom',
-                    max_duration=None,
-                    min_duration=None)
     g.add_goal(gs)
+    gs = TopicMessageParamGoal(topic='robot.on_enter',
+                               name='Bedroom',
+                               condition=lambda msg: True if msg['room'] == "bedroom" else False,
+                               max_duration=5.0,
+                               min_duration=2.0)
     g.add_goal(gs)
     ## More Goals to Generate here
     t.add_goal(g)
